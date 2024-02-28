be0c8e6a3f76d0ee606b2884ccb166d30a7e0e1d
undefined <undefined_1@outlook.com>
2024-02-28 17:06:48 +0800

    初步添加刷课逻辑

--- sibo/__init__.py
+++ sibo/__init__.py
@@ -1,4 +1,16 @@
 from . import api
+from . import errors
 
 if __name__ == "__main__":
-    pass
+    import sys
+    import json
+    configuration_file = sys.argv[1]
+    configuration = json.load(open(configuration_file, 'r'))
+    try:
+        user_id = api.login(configuration["UserName"], configuration["password"], configuration["schoolID"])
+        classes = api.get_classes_id(user_id)
+        print("INFO: ")
+        class_id = classes[0]["id"]
+    except Exception as err:
+        print(f"Error: {err}", file=sys.stderr)
+        exit(1)
