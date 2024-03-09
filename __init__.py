c08247fca856e6397d3500cc3fbafc6c4f7f5e07
undefined <undefined_1@outlook.com>
2024-03-09 16:47:55 +0800

    feat: 尝试添加多线程支持

--- __init__.py
+++ __init__.py
@@ -1,39 +1,99 @@
-import datetime
-from logging import LogRecord
-import loguru
+
 import api, errors
+import loguru
 
-if __name__ == "__main__":
-    import sys
-    import json
-    from loguru import logger
-
-    logger.remove(0) # remove default handler
-    logger.add(sys.stdout, format="[<green>{time:YYYY-MM-DD HH:mm:ss}</green>] [<level>{level}</level>] <level>{message}</level>")
+__all__ = [
+    "api", "errors", "start"
+]
+def __initalize_logger(log_path: str = 'log.log', logger = loguru.logger):
+    logger.remove(0)
+    logger.add(sys.stderr, format="[<green>{time:YYYY-MM-DD HH:mm:ss}</green>] [<level>{level}</level>] <level>{message}</level>")
     logger.add('log.log', format="[{time:YYYY-MM-DD HH:mm:ss}] [{level}] {message}")
-    configuration_file = sys.argv[1]
-    configuration = json.load(open(configuration_file, 'r'))
+    return logger
+
+def __read_configuration_file(configuration_path: str, logger = loguru.logger) -> dict:
     try:
-        logger.info(f"try to login as {configuration['UserName']}")
-        user_id = api.login(configuration["UserName"], configuration["password"], configuration["schoolID"])
-        logger.success("Login Successed.")
+        return json.load(open(configuration_path, 'r'))
+    except Exception as err:
+        logger.critical(f"Failed to read configuration file {configuration_path}: {err}")
+        exit(1)
+
+def start(user_name: str, password: str, school_id: str, number: int = 2, logger = loguru.logger):
+    try:
+        logger.info(f"[{user_name}] try to login as {user_name}")
+        user_id = api.login(user_name, password, school_id)
+        logger.success(f"[{user_name}] Login Successed.")
         classes = api.get_classes_id(user_id)
         class_id = classes[0]["id"]
-        logger.info(f"select class {classes[0]['name']} {class_id}")
-        logger.info("Get All Articales")
+        logger.info(f"[{user_name}] select class {classes[0]['name']} {class_id}")
+        logger.info(f"[{user_name}] Get All Articales")
         articles = api.get_articles(user_id=user_id, class_id=class_id)
-        logger.success(f"The number of Articles is {len(articles)}.")
+        logger.success(f"[{user_name}] The number of Articles is {len(articles)}.")
         i, j = (0 ,0)
-        while j < 2 and i < len(articles):
+        while j < number and i < len(articles):
             try: 
                 api.submit_essay_test(user_id, articles[i], class_id, api.get_essay_answer(articles[i]))
+            except errors.SubmitEssayTestError as err:
+                logger.warning(str(err))
             except Exception as err:
                 logger.error(str(err))
             else:
-                logger.success(f"Succeed submit essay \"{articles[i]['title']}\"")
+                logger.success(f"[{user_name}] Succeed submit essay \"{articles[i]['title']}\"")
                 j += 1
             finally:
                 i += 1
     except Exception as err:
-        logger.error(str(err), file=sys.stderr)
+        logger.error(str(err))
         exit(1)
+
+
+if __name__ == "__main__":
+    import sys
+    import json
+    import threading
+    
+    logger = __initalize_logger()
+    if len(sys.argv) != 2:
+        logger.critical("Invaild Argument.")
+        exit(1)
+        
+    configuration_file_path = sys.argv[1]
+    configuration = __read_configuration_file(configuration_file_path, logger)
+    
+    thread_num: int = configuration.get("threadNumber", 1)
+    logger.info(f"Using {thread_num} threads")
+    
+    threads: list[threading.Thread] = []
+    users: list[dict] = configuration["users"]
+    while len(users):
+        if not len(threads) and len(users):
+            user: dict[str, str] = users.pop()
+            new_thread = threading.Thread(
+                    target=lambda: start(
+                        user_name=user["userName"],
+                        password=user["password"],
+                        school_id=user["schoolID"], 
+                        number=int(user["LessonNumber"]),
+                        logger=logger
+                    )
+                )
+            new_thread.run()
+            threads.append(new_thread)
+        for thread in threads:
+            if not thread.is_alive():
+                threads.remove(thread)
+                if not len(users): continue
+                user: dict[str, str] = users.pop()
+                new_thread = threading.Thread(
+                    target=lambda: start(
+                        user_name=user["userName"],
+                        password=user["password"],
+                        school_id=user["schoolID"], 
+                        number=int(user["LessonNumber"]),
+                        logger=logger
+                    )
+                )
+                new_thread.run()
+                threads.append(new_thread)
+                
+    
