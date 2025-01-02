c7c06766f5700a4e2c5b04a12a2f5854a68c9e88
undefined <undefined_1@outlook.com>
2025-01-02 18:54:29 +0800

    feat(cli): 刷课、CI模式实现, 改进搜索学校输出结果效果

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,53 +1,132 @@
-use std::error::Error;
-use tokio::task::JoinSet;
-use fuck_sibo::cli;
 use fuck_sibo::prelude::*;
+use fuck_sibo::{cli, submit_article};
+use std::io::Write;
+use tokio::task::JoinSet;
 use tracing_log::{log, LogTracer};
-async fn search_school_by_name(name: &str) -> Result<(), Box<dyn Error>> {
-    let schools = search_school(name).await?;
+
+async fn search_school_by_name(name: &str) {
+    let schools = match search_school(name).await {
+        Ok(x) => x,
+        Err(err) => {
+            log::error!("{:#?}", err);
+            return;
+        }
+    };
     schools.into_iter().for_each(|school| {
-        println!("name: {}\nID: {}\n============================", school.name, school.id);
+        println!("name: {}\tID: {}", school.name, school.id);
     });
-    Ok(())
 }
 
-async fn fuck(user: &cli::User) -> Result<(), Box<dyn Error>> {
-    todo!()
+async fn fuck(user: &cli::User) {
+    log::info!("[{}] Try login as {}", user.username, user.username);
+    let user_info = match login(
+        user.username.as_str(),
+        user.password.as_str(),
+        user.school_id.clone().unwrap().as_str(),
+    )
+    .await
+    {
+        Ok(x) => x,
+        Err(err) => {
+            log::error!("[{}] {}", user.username, err);
+            log::trace!("[{}] {:#?}", user.username, err);
+            return;
+        }
+    };
+    log::info!("[{}] Login Successed.", user.username);
+    log::trace!("[{}] {:#?}", user.username, user_info);
+    log::info!("[{}] Get Classes....", user.username);
+    let classes = match get_classes(&user_info.id).await {
+        Ok(x) => x,
+        Err(err) => {
+            log::error!("[{}] {}", user.username, err);
+            log::trace!("[{}] {:#?}", user.username, err);
+            return;
+        }
+    };
+    if classes.is_empty() {
+        log::error!(
+            "[{}] {} does not belong to any class",
+            user.username,
+            user.username
+        );
+        return;
+    }
+    let class = classes.first().unwrap().clone();
+    log::info!("[{}] Crawing articles....", user.username);
+    let articles = match get_articles(&user_info.id, &class.id, None, None).await {
+        Ok(x) => x,
+        Err(err) => {
+            log::error!("[{}] {}", user.username, err);
+            log::trace!("[{}] {:#?}", user.username, err);
+            return;
+        }
+    };
+    let mut successed_num = 0;
+    let total_num = user.number_of_article.unwrap();
+    for article in articles.iter() {
+        if successed_num >= total_num {
+            break;
+        }
+        log::info!("[{}] Attempting article '{}'", user.username, article.title);
+        match submit_article(&user_info.id, &class.id, article, None).await {
+            Ok(_) => {
+                successed_num += 1;
+                log::info!(
+                    "[{}] Successfully submitted article({} / {}) '{}'.",
+                    user.username,
+                    successed_num,
+                    total_num,
+                    article.title,
+                );
+            }
+            Err(err) => {
+                log::warn!(
+                    "[{}] Failed to submit article '{}': {}.",
+                    user.username,
+                    article.title,
+                    err
+                );
+            }
+        };
+    }
+    if successed_num < total_num {
+        log::error!(
+            "[{}] The number of completions is lower than the specified value({} / {})",
+            user.username,
+            successed_num,
+            total_num,
+        );
+    }
 }
-
 
 fn main() -> std::process::ExitCode {
     let mut stderr = std::io::stderr();
-    match tracing::subscriber
-    ::set_global_default(
-        tracing_subscriber::fmt::Subscriber::builder().with_target(false).finish()
-    ) {
-        Err(_) => {
-            let _ = writeln!(&mut stderr, "Failed to set global tracing subscriber");
-            return std::process::ExitCode::FAILURE;
-        },
-        _ => {}
+    if tracing::subscriber::set_global_default(
+        tracing_subscriber::fmt::Subscriber::builder()
+            .with_target(false)
+            .finish(),
+    )
+    .is_err()
+    {
+        let _ = writeln!(&mut stderr, "Failed to set global tracing subscriber");
+        return std::process::ExitCode::FAILURE;
     };
 
-    match LogTracer::builder().init() {
-        Err(_) => {
-            let _ = writeln!(&mut stderr, "Failed to initalize logger");
-            return std::process::ExitCode::FAILURE;
-        },
-        _ => {}
+    if LogTracer::builder().init().is_err() {
+        let _ = writeln!(&mut stderr, "Failed to initalize logger");
+        return std::process::ExitCode::FAILURE;
     };
-
 
     let args = match cli::parse_commandline_arguments() {
         Ok(args) => args,
-        Err(e) => panic!("{}", e)
+        Err(e) => panic!("{}", e),
     };
-    let res = match args.command {
+    match args.command {
         cli::CommandType::Cli => {
             let thread_num: usize = args.thread_num.unwrap_or(1).try_into().unwrap();
             log::info!("Set up as {thread_num} threads");
-            let rt = tokio::runtime::Builder
-            ::new_multi_thread()
+            let rt = tokio::runtime::Builder::new_multi_thread()
                 .worker_threads(thread_num)
                 .enable_all()
                 .build()
@@ -61,43 +140,28 @@ fn main() -> std::process::ExitCode {
                     }
 
                     task_pool.spawn(async move {
-                        match fuck(&user).await {
-                            Err(e) => {
-                                let _ = writeln!(&mut stderr, "{}", e);
-                            },
-                            _ => {}
-                        };
+                        fuck(&user).await;
                     });
                 }
 
-                while task_pool.len() > 0 {
+                while !task_pool.is_empty() {
                     task_pool.join_next().await;
                 }
-
-            });
-        },
-        cli::CommandType::SearchSchool => {
-            let rt = tokio::runtime::Builder::new_current_thread().enable_all().build().unwrap();
-            rt.block_on(async {
-                match search_school_by_name(&args.school_name.unwrap()).await {
-                    Err(e) => {
-                        let _ = writeln!(&mut stderr, "{}", e);
-                    },
-                    _ => {}
-                }
-            });
-            
-        },
-        cli::CommandType::Tui => {
-            unimplemented!("暂未实现")
-
+                std::process::ExitCode::SUCCESS
+            })
         }
-    };
-
-    match res {
-        Err(_) => std::process::ExitCode::FAILURE,
-        _ => std::process::ExitCode::SUCCESS
+        cli::CommandType::SearchSchool => {
+            let rt = tokio::runtime::Builder::new_current_thread()
+                .enable_all()
+                .build()
+                .unwrap();
+            rt.block_on(async {
+                search_school_by_name(&args.school_name.unwrap()).await;
+                std::process::ExitCode::SUCCESS
+            })
+        }
+        cli::CommandType::Tui => {
+            unimplemented!("暂未实现");
+        }
     }
-
 }
-
